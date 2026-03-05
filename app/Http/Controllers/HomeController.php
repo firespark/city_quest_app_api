fec05cbda8f5d13be648e08a008d535506db6142
firespark <fireleit@gmail.com>
2026-03-05 18:14:25 +0400

    fix: add HomeController

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Mail;
+use App\Models\Task;
+use App\Models\Sight;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\URL;
+
+class HomeController extends Controller
+{
+
+    public function index()
+    {
+    }
+}
